7ab35b9bbed00a48f6929735f68f2f6da08204f9
norbusonam <nsonam1822@gmail.com>
2023-03-05 20:26:36 -0800

    prisma setup

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import { apiRouter } from "./routers";
+import { PrismaClient } from "@prisma/client";
 
+// load environment variables from .env file
 dotenv.config();
 
+// configure server
 const app = express();
-const port = process.env.PORT;
-
+app.use(express.json());
 app.use("/api", apiRouter);
 
-console.log("Hello World");
+// start server
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
---
+++ backend/prisma/migrations/20230306041235_init/migration.sql
@@ -0,0 +1,30 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "due" TIMESTAMP(3) NOT NULL,
+    "creatorId" TEXT NOT NULL,
+    "created" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Todo" ADD CONSTRAINT "Todo_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
