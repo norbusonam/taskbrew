9300bcb1e96976103ded334b3cb955be7ab7adb5
norbusonam <nsonam1822@gmail.com>
2023-03-08 22:34:55 -0800

    cleaned up linking

--- frontend/src/components/navbar/navbar.tsx
+++ frontend/src/components/navbar/navbar.tsx
@@ -5,22 +5,24 @@ export const Navbar: React.FC = () => {
   return (
     <nav className="navbar bg-base-200 rounded-xl border-2 border-base-100 shadow-lg">
       <div className="navbar-start">
-        <Link to={'/'} className="btn btn-ghost normal-case text-xl">
+        <Link to="/" className="btn btn-ghost normal-case text-xl">
           todo
         </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <Link to={'about'}>About Us</Link>
+            <Link to="/about">About Us</Link>
           </li>
           <li>
-            <Link to={'pricing'}>Pricing</Link>
+            <Link to="/pricing">Pricing</Link>
           </li>
         </ul>
       </div>
       <div className="navbar-end">
-        <button className="btn">Get started</button>
+        <Link to="/signup" className="btn">
+          Get started
+        </Link>
       </div>
     </nav>
   );
