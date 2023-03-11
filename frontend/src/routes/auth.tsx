85b9faeb187f36f65372d4766acc733ad56e54ac
norbusonam <nsonam1822@gmail.com>
2023-03-11 09:20:52 -0800

    press enter to submit form

--- frontend/src/routes/auth.tsx
+++ frontend/src/routes/auth.tsx
@@ -14,6 +14,15 @@ export const Auth: React.FC<AuthProps> = props => {
   const [passwordConfirmation, setPasswordConfirmation] = useState('');
   const { onAuthenticated } = useAuth();
 
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (props.type === 'login') {
+      onLogin();
+    } else {
+      onSignup();
+    }
+  };
+
   const onSignup = () => {
     api
       .signup({ email, name, password })
@@ -45,55 +54,57 @@ export const Auth: React.FC<AuthProps> = props => {
             </Link>
           </div>
           <div className="card-title">{props.type === 'login' ? 'Login' : 'Signup'}</div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">What's your email?</span>
-            </label>
-            <input
-              type="email"
-              placeholder="john@email.com"
-              className="input input-bordered"
-              onChange={e => setEmail(e.target.value)}
-            />
-          </div>
-          <div className={`form-control ${props.type === 'login' && 'hidden'}`}>
-            <label className="label">
-              <span className="label-text">What's your name?</span>
-            </label>
-            <input
-              type="text"
-              placeholder="John Doe"
-              className="input input-bordered w-full"
-              onChange={e => setName(e.target.value)}
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Enter a password</span>
-            </label>
-            <input
-              type="password"
-              placeholder="************"
-              className="input input-bordered w-full"
-              onChange={e => setPassword(e.target.value)}
-            />
-          </div>
-          <div className={`form-control ${props.type === 'login' && 'hidden'}`}>
-            <label className="label">
-              <span className="label-text">Confirm password</span>
-            </label>
-            <input
-              type="password"
-              placeholder="************"
-              className={`input input-bordered w-full ${password !== passwordConfirmation && 'input-error'}}`}
-              onChange={e => setPasswordConfirmation(e.target.value)}
-            />
-          </div>
-          <div className="card-actions justify-end pt-2">
-            <button className="btn btn-primary" onClick={() => (props.type === 'login' ? onLogin() : onSignup())}>
-              {props.type === 'login' ? 'Login' : 'Signup'}
-            </button>
-          </div>
+          <form onSubmit={onSubmit}>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">What's your email?</span>
+              </label>
+              <input
+                type="email"
+                placeholder="john@email.com"
+                className="input input-bordered"
+                onChange={e => setEmail(e.target.value)}
+              />
+            </div>
+            <div className={`form-control ${props.type === 'login' && 'hidden'}`}>
+              <label className="label">
+                <span className="label-text">What's your name?</span>
+              </label>
+              <input
+                type="text"
+                placeholder="John Doe"
+                className="input input-bordered w-full"
+                onChange={e => setName(e.target.value)}
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Enter a password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="************"
+                className="input input-bordered w-full"
+                onChange={e => setPassword(e.target.value)}
+              />
+            </div>
+            <div className={`form-control ${props.type === 'login' && 'hidden'}`}>
+              <label className="label">
+                <span className="label-text">Confirm password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="************"
+                className={`input input-bordered w-full ${password !== passwordConfirmation && 'input-error'}}`}
+                onChange={e => setPasswordConfirmation(e.target.value)}
+              />
+            </div>
+            <div className="card-actions justify-end pt-2">
+              <button type="submit" className="btn btn-primary">
+                {props.type === 'login' ? 'Login' : 'Signup'}
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
