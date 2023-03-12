aeaed0db5dbac56d92d5438227d47caaf69bee11
norbusonam <nsonam1822@gmail.com>
2023-03-11 16:40:56 -0800

    redirect to home on landing page if authenticated

--- frontend/src/routes/landing.tsx
+++ frontend/src/routes/landing.tsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../hooks';
 
 export const Landing: React.FC = () => {
+  const navigate = useNavigate();
+  const { isAuthenticated } = useAuth();
+
+  // if user is already authenticated, redirect to home
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/home');
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div>
       <h1>Landing</h1>
