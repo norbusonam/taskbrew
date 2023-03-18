1e2a9e364a9e9ca14680c46c33b0bf8ff302bbe0
norbusonam <nsonam1822@gmail.com>
2023-03-18 14:14:58 -0700

    use viewport hook for height/width tracking

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useAuth } from './use-auth';
 export { useTheme } from './use-theme';
+export { useViewport } from './use-viewport';
 export { useAppDispatch, useAppSelector } from './use-redux';
