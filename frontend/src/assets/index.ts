d9bfece85a55ea6e76454952a4d5580d167446c0
norbusonam <nsonam1822@gmail.com>
2023-03-17 20:45:49 -0700

    new arrow assets

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -2,5 +2,7 @@ export { ReactComponent as PaintBrushIcon } from './paint-brush.svg';
 export { ReactComponent as CheckIcon } from './check.svg';
 export { ReactComponent as ArrowRightIcon } from './arrow-right.svg';
 export { ReactComponent as ArrowLeftIcon } from './arrow-left.svg';
+export { ReactComponent as ArrowDoubleRightIcon } from './arrow-double-right.svg';
+export { ReactComponent as ArrowDoubleLeftIcon } from './arrow-double-left.svg';
 export { ReactComponent as CoffeeIcon } from './coffee.svg';
 export { ReactComponent as HomeIcon } from './home.svg';
