306b86dfdd169a1054038fbb2c5e16e48fce919e
norbusonam <nsonam1822@gmail.com>
2023-03-25 11:40:44 -0700

    list redux setup

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../slices/auth-slice';
 import taskReducer from '../slices/task-slice';
+import listReducer from '../slices/list-slice';
 import toastReducer from '../slices/toast-slice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     task: taskReducer,
+    list: listReducer,
     toast: toastReducer,
   },
 });
