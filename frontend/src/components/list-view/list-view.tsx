1b87e2ae521f6e252f291b0500a3434df89c11d8
norbusonam <nsonam1822@gmail.com>
2023-03-26 11:47:01 -0700

    removed unnecessary state, imports, fixed sort bug

--- frontend/src/components/list-view/list-view.tsx
+++ frontend/src/components/list-view/list-view.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import { api } from '../../api';
+import React, { useEffect } from 'react';
 import { useLists } from '../../hooks';
 import { Task } from '../../types';
 import { CreateListButton } from './create-list-button';
@@ -11,7 +10,6 @@ type ListViewProps = {
 
 export const ListView: React.FC<ListViewProps> = props => {
   const { lists, getAllLists, createList } = useLists();
-  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     getAllLists();
@@ -27,14 +25,13 @@ export const ListView: React.FC<ListViewProps> = props => {
 
   return (
     <div>
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : lists && lists.length === 0 ? (
+      {lists && lists.length === 0 ? (
         <div>No lists 😢</div>
       ) : (
         <div className="flex flex-row w-full gap-2 p-2">
           {lists &&
             lists
+              .slice()
               .sort((a, b) => a.order - b.order)
               .map(list => <ListViewList key={list.id} list={list} tasks={props.tasks} />)}
         </div>
