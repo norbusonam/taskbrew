2fedcac16eeadca5a08f54453c9606293828f658
norbusonam <nsonam1822@gmail.com>
2023-10-01 00:39:16 -0700

    full height sidebar, setting and sign out buttons

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -72,24 +72,83 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex">
       {/* sidebar */}
-      <div className="m-4 w-64 rounded-2xl bg-gray-200 p-4">
-        <h2 className="text-xl font-bold">Menu</h2>
-        <h4 className="py-2 text-xs font-bold">TASKS</h4>
-        <ul className="font-light">
-          {ROUTES.map((route) => (
-            <li key={route.path}>
-              <Link
-                className={`flex flex-row items-center gap-2 rounded-md px-2 py-1 transition-colors hover:bg-gray-300 active:bg-gray-400 ${
-                  pathname === route.path && "bg-gray-300"
-                }`}
-                href={route.path}
+      <div className="h-screen p-4">
+        <div className="flex h-full w-64 flex-col justify-between rounded-2xl bg-gray-200 p-4">
+          <div className="overflow-scroll">
+            <div className="flex items-center gap-2">
+              <svg
+                className="h-6 w-6"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                {route.icon}
-                <p>{route.name}</p>
-              </Link>
-            </li>
-          ))}
-        </ul>
+                <g id="Environment / Coffee">
+                  <path
+                    id="Vector"
+                    d="M4 20H10.9433M10.9433 20H11.0567M10.9433 20C10.9622 20.0002 10.9811 20.0002 11 20.0002C11.0189 20.0002 11.0378 20.0002 11.0567 20M10.9433 20C7.1034 19.9695 4 16.8468 4 12.9998V8.92285C4 8.41305 4.41305 8 4.92285 8H17.0767C17.5865 8 18 8.41305 18 8.92285V9M11.0567 20H18M11.0567 20C14.8966 19.9695 18 16.8468 18 12.9998M18 9H19.5C20.8807 9 22 10.1193 22 11.5C22 12.8807 20.8807 14 19.5 14H18V12.9998M18 9V12.9998M15 3L14 5M12 3L11 5M9 3L8 5"
+                    stroke="#000000"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </g>
+              </svg>
+              <h2 className="text-xl font-bold">Taskbrew</h2>
+            </div>
+            <h4 className="py-2 text-xs font-bold">TASKS</h4>
+            <ul className="font-light">
+              {ROUTES.map((route) => (
+                <li key={route.path}>
+                  <Link
+                    className={`flex flex-row items-center gap-2 rounded-md px-2 py-1 transition-colors hover:bg-gray-300 active:bg-gray-400 ${
+                      pathname === route.path && "bg-gray-300"
+                    }`}
+                    href={route.path}
+                  >
+                    {route.icon}
+                    <p>{route.name}</p>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="border-t-[1px] border-gray-300 pt-2">
+            <button className="flex w-full flex-row items-center gap-2 rounded-md px-2 py-1 font-light transition-colors hover:bg-gray-300 active:bg-gray-400">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75"
+                />
+              </svg>
+              <p>Settings</p>
+            </button>
+            <button className="flex w-full flex-row items-center gap-2 rounded-md px-2 py-1 font-light transition-colors hover:bg-gray-300 active:bg-gray-400">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
+                />
+              </svg>
+              <p>Sign out</p>
+            </button>
+          </div>
+        </div>
       </div>
 
       {/* page */}
