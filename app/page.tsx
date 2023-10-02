767e8c19a40507c98773d6480adfeb3340b9e39d
norbusonam <nsonam1822@gmail.com>
2023-10-02 00:05:15 -0700

    bare bones home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Hello, World!</div>;
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-4">
+      <h1 className="text-lg">Welcome to Taskbrew</h1>
+      <Link
+        className="flex w-64 items-center justify-center gap-2 rounded-md bg-gray-300 px-4 py-2 font-light transition-colors hover:bg-gray-400 active:bg-gray-500"
+        href="/auth"
+      >
+        Get Started
+      </Link>
+    </div>
+  );
 }
