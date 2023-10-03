af81f080933e7d621bf91795e1a5e108eb7bedcb
norbusonam <nsonam1822@gmail.com>
2023-10-03 16:27:57 -0700

    check session on update

--- app/api/task/[id]/route.ts
+++ app/api/task/[id]/route.ts
@@ -1,13 +1,23 @@
 import prisma from "@taskbrew/prisma/db";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/route";
 
 export async function PATCH(
   req: Request,
   { params }: { params: { id: string } },
 ) {
+  // check session
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user || !session.user.id) {
+    return Response.redirect("/auth");
+  }
+
+  // update task
   const body = await req.json();
-  const task = await prisma.task.update({
+  const updatedTask = await prisma.task.update({
     where: {
       id: params.id,
+      userId: session.user.id,
     },
     data: {
       title: body.title,
@@ -16,5 +26,6 @@ export async function PATCH(
       duration: body.duration,
     },
   });
-  return Response.json(task);
+
+  return Response.json(updatedTask);
 }
