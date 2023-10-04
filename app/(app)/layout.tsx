8a3986fe8d6ead14a9fbe48d179d702a09a9a97e
norbusonam <nsonam1822@gmail.com>
2023-10-03 20:50:31 -0700

    central icon module based on ant design

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,5 +1,13 @@
 "use client";
 
+import {
+  IconCalendar,
+  IconClockCircle,
+  IconCoffee,
+  IconRocket,
+  IconSetting,
+  IconUser,
+} from "@taskbrew/components/icons";
 import { SignOutButton } from "@taskbrew/components/sign-out-button";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
@@ -10,62 +18,17 @@ const ROUTES = [
   {
     name: "Today",
     path: "/today",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="h-4 w-4"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 010 3.75H5.625a1.875 1.875 0 010-3.75z"
-        />
-      </svg>
-    ),
+    icon: <IconRocket className="h-6 w-6" />,
   },
   {
     name: "Upcoming",
     path: "/upcoming",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="h-4 w-4"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M3 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062A1.125 1.125 0 013 16.81V8.688zM12.75 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062a1.125 1.125 0 01-1.683-.977V8.688z"
-        />
-      </svg>
-    ),
+    icon: <IconClockCircle className="h-6 w-6" />,
   },
   {
     name: "Calendar",
     path: "/calendar",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="h-4 w-4"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5"
-        />
-      </svg>
-    ),
+    icon: <IconCalendar className="h-6 w-6" />,
   },
 ];
 
@@ -79,32 +42,16 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="h-screen p-4">
         <div className="flex h-full w-64 flex-col justify-between rounded-2xl bg-gray-200 p-4">
           <div className="overflow-scroll">
-            <div className="flex items-center gap-2">
-              <svg
-                className="h-6 w-6"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g id="Environment / Coffee">
-                  <path
-                    id="Vector"
-                    d="M4 20H10.9433M10.9433 20H11.0567M10.9433 20C10.9622 20.0002 10.9811 20.0002 11 20.0002C11.0189 20.0002 11.0378 20.0002 11.0567 20M10.9433 20C7.1034 19.9695 4 16.8468 4 12.9998V8.92285C4 8.41305 4.41305 8 4.92285 8H17.0767C17.5865 8 18 8.41305 18 8.92285V9M11.0567 20H18M11.0567 20C14.8966 19.9695 18 16.8468 18 12.9998M18 9H19.5C20.8807 9 22 10.1193 22 11.5C22 12.8807 20.8807 14 19.5 14H18V12.9998M18 9V12.9998M15 3L14 5M12 3L11 5M9 3L8 5"
-                    stroke="#000000"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </g>
-              </svg>
-              <h2 className="text-xl font-bold">Taskbrew</h2>
+            <div className="flex gap-2">
+              <IconCoffee className="h-6 w-6" />
+              <h2 className="text-xl font-medium">Taskbrew</h2>
             </div>
             <h4 className="py-2 text-xs font-bold">TASKS</h4>
             <ul className="font-light">
               {ROUTES.map((route) => (
                 <li key={route.path}>
                   <Link
-                    className={`flex flex-row items-center gap-2 rounded-md px-2 py-1 transition-colors hover:bg-gray-300 active:bg-gray-400 ${
+                    className={`flex flex-row items-center gap-2 rounded-md p-2 transition-colors hover:bg-gray-300 active:bg-gray-400 ${
                       pathname === route.path && "bg-gray-300"
                     }`}
                     href={route.path}
@@ -118,7 +65,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           </div>
           <div className="border-t-[1px] border-gray-300 pt-2">
             {session.status === "authenticated" && session.data.user && (
-              <button className="flex w-full flex-row items-center gap-2 rounded-md px-2 py-1 font-light transition-colors hover:bg-gray-300 active:bg-gray-400">
+              <button className="flex w-full flex-row items-center gap-2 rounded-md p-2 font-light transition-colors hover:bg-gray-300 active:bg-gray-400">
                 {session.data.user.image ? (
                   <Image
                     className="h-6 w-6 rounded-full"
@@ -128,39 +75,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                     alt="Profile picture"
                   />
                 ) : (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="h-6 w-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-                    />
-                  </svg>
+                  <IconUser className="h-6 w-6" />
                 )}
                 <p>Account</p>
               </button>
             )}
-            <button className="flex w-full flex-row items-center gap-2 rounded-md px-2 py-1 font-light transition-colors hover:bg-gray-300 active:bg-gray-400">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="h-6 w-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75"
-                />
-              </svg>
+            <button className="flex w-full flex-row items-center gap-2 rounded-md p-2 font-light transition-colors hover:bg-gray-300 active:bg-gray-400">
+              <IconSetting className="h-6 w-6" />
               <p>Settings</p>
             </button>
             <SignOutButton />
--- app/(app)/upcoming/page.tsx
+++ app/(app)/upcoming/page.tsx
@@ -2,6 +2,7 @@ import prisma from "@taskbrew/prisma/db";
 import { TaskList } from "@taskbrew/components/task-list";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@taskbrew/app/api/auth/[...nextauth]/route";
+import { IconPlus } from "@taskbrew/components/icons";
 
 export default async function Page() {
   const session = await getServerSession(authOptions);
@@ -27,20 +28,7 @@ export default async function Page() {
 
       {/* add new task */}
       <div className="mr-4 flex flex-row items-center gap-2 border-b-[1px] border-gray-200 p-2 text-gray-500 transition-all hover:cursor-pointer hover:rounded-md hover:bg-gray-200 active:bg-gray-300">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="h-4 w-4"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M12 4.5v15m7.5-7.5h-15"
-          />
-        </svg>
+        <IconPlus className="h-4 w-4" />
         <span>New task</span>
       </div>
     </div>
