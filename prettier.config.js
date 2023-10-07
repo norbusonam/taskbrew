a955d10799779cbd0dd77ad722122d4052f70f33
norbusonam <nsonam1822@gmail.com>
2023-10-07 03:42:59 -0700

    tailwind prettier plugin must be last

--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  plugins: ["prettier-plugin-tailwindcss", "prettier-plugin-organize-imports"],
+  // prettier-plugin-tailwindcss must be loaded last to prevent conflicts with other plugins:
+  // https://github.com/tailwindlabs/prettier-plugin-tailwindcss#compatibility-with-other-prettier-plugins
+  plugins: ["prettier-plugin-organize-imports", "prettier-plugin-tailwindcss"],
 };
