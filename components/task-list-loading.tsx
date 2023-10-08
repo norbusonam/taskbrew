1d28eedf49999adc33b759302b49b5cdb6b3447b
norbusonam <nsonam1822@gmail.com>
2023-10-08 11:44:39 -0700

    fixed loading skeleton on smaller screens

--- components/task-list-loading.tsx
+++ components/task-list-loading.tsx
@@ -1,4 +1,11 @@
-const widths = ["w-64", "w-60", "w-72", "w-60", "w-80", "w-64"];
+const widths = [
+  "basis-64",
+  "basis-60",
+  "basis-72",
+  "basis-60",
+  "basis-80",
+  "basis-64",
+];
 
 export function TaskListLoading() {
   return (
@@ -6,16 +13,18 @@ export function TaskListLoading() {
       {widths.map((width, i) => (
         <div
           key={i}
-          className="mr-4 flex flex-row items-center gap-2 border-b-[1px] border-gray-200 p-2"
+          className="flex flex-row items-center gap-2 border-b-[1px] border-gray-200 p-2"
         >
-          <div className="h-5 w-5 animate-pulse rounded-md bg-gray-300"></div>
-          <div className="w-full space-y-2">
-            <div
-              className={`h-5 ${width} max-w-full animate-pulse rounded-md bg-gray-300`}
-            ></div>
+          <div className="h-5 w-5 shrink-0 animate-pulse rounded-md bg-gray-300" />
+          <div className="basis-full space-y-2">
+            <div className="flex">
+              <div
+                className={`h-5 ${width} animate-pulse rounded-md bg-gray-300`}
+              />
+            </div>
             <div className="flex gap-1">
-              <div className="h-5 w-24 animate-pulse rounded-md bg-gray-300"></div>
-              <div className="h-5 w-24 animate-pulse rounded-md bg-gray-300"></div>
+              <div className="h-5 basis-24 animate-pulse rounded-md bg-gray-300" />
+              <div className="h-5 basis-24 animate-pulse rounded-md bg-gray-300" />
             </div>
           </div>
         </div>
