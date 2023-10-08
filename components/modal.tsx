67560045a594a55afed1b717d821abcdc454a6bf
norbusonam <nsonam1822@gmail.com>
2023-10-08 16:54:40 -0700

    unnecessary text center

--- components/modal.tsx
+++ components/modal.tsx
@@ -27,7 +27,7 @@ export function Modal(props: Props) {
           <div className="fixed inset-0 bg-black bg-opacity-40" />
         </Transition.Child>
         <div className="fixed inset-0 overflow-y-auto">
-          <div className="flex min-h-full items-center justify-center p-4 text-center">
+          <div className="flex min-h-full items-center justify-center p-4">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
