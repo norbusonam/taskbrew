2e1b6d39c41c4fd1825ed51af0d6b89f3b88283c
norbusonam <nsonam1822@gmail.com>
2023-10-11 19:30:20 -0700

    fix weird nextjs/webapi bugs

--- hooks/use-theme.ts
+++ hooks/use-theme.ts
@@ -1,32 +1,39 @@
 import { useEffect, useState } from "react";
 
 export function useTheme() {
-  const [theme, setTheme] = useState<"light" | "dark" | "system">(
-    localStorage.getItem("theme") === "dark"
-      ? "dark"
-      : localStorage.getItem("theme") === "light"
-      ? "light"
-      : "system",
-  );
+  const [theme, setTheme] = useState<"light" | "dark" | "system">("system");
+  const [isInitialized, setIsInitialized] = useState(false);
 
   useEffect(() => {
-    if (theme === "dark") {
-      // explicitly set dark mode
-      localStorage.setItem("theme", "dark");
-      document.documentElement.classList.add("dark");
-    } else if (theme === "light") {
-      // explicitly set light mode
-      localStorage.setItem("theme", "light");
-      document.documentElement.classList.remove("dark");
-    } else if (theme === "system") {
-      // explicity or implicitly set auto mode
-      localStorage.removeItem("theme");
-      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    const localTheme = localStorage.getItem("theme");
+    if (localTheme === "dark" || localTheme === "light") {
+      setTheme(localTheme as "light" | "dark");
+    }
+    setIsInitialized(true);
+  }, []);
+
+  useEffect(() => {
+    if (isInitialized) {
+      if (theme === "dark") {
+        // explicitly set dark mode
+        localStorage.setItem("theme", "dark");
         document.documentElement.classList.add("dark");
-      } else {
+      } else if (theme === "light") {
+        // explicitly set light mode
+        localStorage.setItem("theme", "light");
         document.documentElement.classList.remove("dark");
+      } else if (theme === "system") {
+        // explicity or implicitly set auto mode
+        localStorage.removeItem("theme");
+        if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+          document.documentElement.classList.add("dark");
+        } else {
+          document.documentElement.classList.remove("dark");
+        }
       }
     }
+    // only needs to run on theme change
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [theme]);
 
   return {
