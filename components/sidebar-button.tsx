347a97f3346e3dcfd0a14bce8549cef57ae2a00a
norbusonam <nsonam1822@gmail.com>
2023-10-11 20:14:15 -0700

    fixed active sidebar styles

--- components/sidebar-button.tsx
+++ components/sidebar-button.tsx
@@ -11,7 +11,7 @@ export function SidebarButton(props: Props) {
     <button
       onClick={props.onClick}
       className={`flex w-full flex-row items-center justify-center gap-2 rounded-md p-2 transition-colors hover:bg-neutral-200 active:bg-neutral-300 dark:hover:bg-neutral-800 dark:active:bg-neutral-700 md:justify-start ${
-        props.active && "bg-neutral-300 dark:bg-neutral-700"
+        props.active && "bg-neutral-200 dark:bg-neutral-800"
       } ${props.className}`}
     >
       {props.icon}
