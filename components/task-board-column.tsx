d027ed6c5191d5d10f76cc12c7ab1b0feb7573ce
norbusonam <nsonam1822@gmail.com>
2023-10-16 07:16:31 -0700

    task number subtitle

--- components/task-board-column.tsx
+++ components/task-board-column.tsx
@@ -49,15 +49,20 @@ export function TaskBoardColumn(props: Props) {
   };
 
   return (
-    <div className="max-w flex h-fit min-w-[18rem] max-w-[24rem] flex-1 flex-col justify-between gap-2 rounded-md bg-neutral-100 p-4 shadow-md dark:bg-neutral-900">
+    <div className="flex h-fit min-w-[18rem] max-w-[24rem] flex-1 flex-col justify-between gap-2 rounded-md bg-neutral-100 p-4 shadow-md dark:bg-neutral-900">
       <div className="flex flex-col gap-2">
-        <h2 className="text-lg font-medium">
-          {props.type === "NOT_STARTED"
-            ? "Not started"
-            : props.type === "IN_PROGRESS"
-            ? "In progress"
-            : "Completed"}
-        </h2>
+        <div>
+          <h2 className="text-lg font-medium">
+            {props.type === "NOT_STARTED"
+              ? "Not started"
+              : props.type === "IN_PROGRESS"
+              ? "In progress"
+              : "Completed"}
+          </h2>
+          <span className="text-sm text-neutral-500">
+            {props.tasks.length} tasks
+          </span>
+        </div>
         <SortableContext items={taskIds}>
           {props.tasks.length > 0 ? (
             props.tasks.map((task) => (
