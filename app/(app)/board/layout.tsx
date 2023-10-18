33e6e5ae08bf4a7480adea74bc284444dec81a6b
norbusonam <nsonam1822@gmail.com>
2023-10-17 19:38:46 -0700

    remove unnecessary overflow-y-scroll

--- app/(app)/board/layout.tsx
+++ app/(app)/board/layout.tsx
@@ -1,6 +1,6 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen w-full flex-col overflow-y-scroll">
+    <div className="flex h-screen w-full flex-col">
       {/* header */}
       <div className="p-4">
         <h1 className="text-4xl font-bold">Board</h1>
