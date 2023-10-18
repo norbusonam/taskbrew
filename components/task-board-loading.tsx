291be608737e49258c9161977d389d7f26c52c8e
norbusonam <nsonam1822@gmail.com>
2023-10-17 19:41:00 -0700

    realign board loading skeleton

--- components/task-board-loading.tsx
+++ components/task-board-loading.tsx
@@ -3,7 +3,7 @@ export function TaskBoardLoading() {
     <div className="flex gap-4 px-4">
       <div className="flex w-full gap-4">
         <div className="flex min-w-[18rem] max-w-[24rem] flex-1 flex-col gap-4">
-          <div className="flex h-fit flex-col gap-2 rounded-md bg-neutral-100 p-4 shadow-md dark:bg-neutral-900">
+          <div className="flex h-fit flex-col gap-2 rounded-md bg-neutral-100 p-2 shadow-md dark:bg-neutral-900">
             <div>
               <h2 className="text-lg font-medium">Not started</h2>
               <span className="text-sm text-neutral-500">4 tasks</span>
@@ -14,7 +14,7 @@ export function TaskBoardLoading() {
           </div>
         </div>
         <div className="flex min-w-[18rem] max-w-[24rem] flex-1 flex-col gap-4">
-          <div className="flex h-fit flex-col gap-2 rounded-md bg-neutral-100 p-4 shadow-md dark:bg-neutral-900">
+          <div className="flex h-fit flex-col gap-2 rounded-md bg-neutral-100 p-2 shadow-md dark:bg-neutral-900">
             <div>
               <h2 className="text-lg font-medium">In progress</h2>
               <span className="text-sm text-neutral-500">3 tasks</span>
@@ -25,7 +25,7 @@ export function TaskBoardLoading() {
           </div>
         </div>
         <div className="flex min-w-[18rem] max-w-[24rem] flex-1 flex-col gap-4">
-          <div className="flex h-fit flex-col gap-2 rounded-md bg-neutral-100 p-4 shadow-md dark:bg-neutral-900">
+          <div className="flex h-fit flex-col gap-2 rounded-md bg-neutral-100 p-2 shadow-md dark:bg-neutral-900">
             <div>
               <h2 className="text-lg font-medium">Completed</h2>
               <span className="text-sm text-neutral-500">5 tasks</span>
