1e9ca4ff80f6cac16bd41c0238f06c50eae3a4fb
norbusonam <nsonam1822@gmail.com>
2023-10-20 15:55:29 -0700

    fix horizontal scroll regression

--- app/(app)/board/layout.tsx
+++ app/(app)/board/layout.tsx
@@ -1,6 +1,6 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen w-full flex-col">
+    <div className="flex h-screen w-full flex-col overflow-hidden">
       {/* header */}
       <div className="p-4">
         <h1 className="text-4xl font-bold">Board</h1>
