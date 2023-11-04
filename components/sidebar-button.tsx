0b0eeb40193baceab788e709c93e3613eafe0748
norbusonam <nsonam1822@gmail.com>
2023-11-04 01:13:58 -0700

    let parent decide when to hide text, center items

--- components/sidebar-button.tsx
+++ components/sidebar-button.tsx
@@ -1,6 +1,7 @@
 type Props = {
   icon: React.ReactNode;
   text: string;
+  hideText?: boolean;
   className?: string;
   active?: boolean;
   onClick?: () => void;
@@ -10,12 +11,12 @@ export function SidebarButton(props: Props) {
   return (
     <button
       onClick={props.onClick}
-      className={`flex w-full flex-row items-center justify-center gap-2 rounded-md p-2 transition-colors hover:bg-neutral-200 active:bg-neutral-300 dark:hover:bg-neutral-800 dark:active:bg-neutral-700 md:justify-start ${
+      className={`flex w-full flex-row items-center justify-start gap-2 rounded-md p-2 transition-colors hover:bg-neutral-200 active:bg-neutral-300 dark:hover:bg-neutral-800 dark:active:bg-neutral-700 ${
         props.active && "bg-neutral-200 dark:bg-neutral-800"
-      } ${props.className}`}
+      } ${props.hideText && "justify-center"} ${props.className}`}
     >
       {props.icon}
-      <p className="hidden md:block">{props.text}</p>
+      <p className={`${props.hideText && "hidden"}`}>{props.text}</p>
     </button>
   );
 }
