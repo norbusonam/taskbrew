3563aac7170e8023143ae34981d3ce95af9942b9
norbusonam <nsonam1822@gmail.com>
2023-11-20 17:11:46 -0500

    editable title and notes in modal

--- components/modal/task-modal-content.tsx
+++ components/modal/task-modal-content.tsx
@@ -1,15 +1,47 @@
 import { Task } from "@taskbrew/prisma/db";
+import {
+  UpdateTaskBody,
+  updateTask,
+} from "@taskbrew/server-actions/update-task";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { EditableText } from "../editable-text";
 
 type Props = {
   task: Task;
 };
 
 export function TaskModalContent(props: Props) {
+  const [optimisticTask, setOptimisticTask] = useState(props.task);
+
+  const onUpdateTask = (body: UpdateTaskBody) => {
+    toast
+      .promise(updateTask(props.task.id, body), {
+        loading: "Updating task...",
+        success: "Task updated!",
+        error: "Failed to update task",
+      })
+      .catch(() => {
+        setOptimisticTask(props.task);
+      });
+  };
+
   return (
     <div>
-      <p>{props.task.title}</p>
-      <p>{props.task.status}</p>
-      <p>{props.task.notes}</p>
+      <EditableText
+        text={optimisticTask.title}
+        onTextChanged={(title) => {
+          setOptimisticTask((prev) => ({ ...prev, title }));
+          onUpdateTask({ title });
+        }}
+      />
+      <EditableText
+        text={optimisticTask.notes}
+        onTextChanged={(notes) => {
+          setOptimisticTask((prev) => ({ ...prev, notes }));
+          onUpdateTask({ notes });
+        }}
+      />
     </div>
   );
 }
