276a57d05a03d5fa4b9ce650001fcc20ffc5d1c9
norbusonam <nsonam1822@gmail.com>
2023-11-22 19:31:46 -0500

    move get tasks to server action

--- app/(app)/list/page.tsx
+++ app/(app)/list/page.tsx
@@ -1,6 +1,6 @@
 import { authOptions } from "@taskbrew/app/api/auth/auth-options";
 import { TaskList } from "@taskbrew/components/task-list";
-import prisma, { Task } from "@taskbrew/prisma/db";
+import { getTasksByFilter } from "@taskbrew/server-actions/get-tasks";
 import { Metadata } from "next";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 export default async function Page({
   searchParams,
 }: {
-  searchParams?: { [key: string]: string | string[] | undefined };
+  searchParams?: { [key: string]: string | undefined };
 }) {
   const session = await getServerSession(authOptions);
 
@@ -20,78 +20,7 @@ export default async function Page({
     redirect("/auth");
   }
 
-  let tasks: Task[] = [];
-  if (searchParams?.filter === "completed") {
-    tasks = await prisma.task.findMany({
-      where: {
-        userId: session.user.id,
-        status: "COMPLETED",
-      },
-      orderBy: {
-        completedAt: "desc",
-      },
-    });
-  } else if (searchParams?.filter === "noDueDate") {
-    tasks = await prisma.task.findMany({
-      where: {
-        userId: session.user.id,
-        status: {
-          not: "COMPLETED",
-        },
-        dueDate: null,
-      },
-      orderBy: {
-        listOrder: "asc",
-      },
-    });
-  } else if (searchParams?.filter === "overdue") {
-    tasks = await prisma.task.findMany({
-      where: {
-        userId: session.user.id,
-        status: {
-          not: "COMPLETED",
-        },
-        dueDate: {
-          lt: new Date(new Date().setHours(0, 0, 0, 0)),
-        },
-      },
-      orderBy: {
-        listOrder: "asc",
-      },
-    });
-  } else if (searchParams?.filter === "upcoming") {
-    tasks = await prisma.task.findMany({
-      where: {
-        userId: session.user.id,
-        status: {
-          not: "COMPLETED",
-        },
-        dueDate: {
-          gt: new Date(),
-        },
-      },
-      orderBy: {
-        listOrder: "asc",
-      },
-    });
-  } else {
-    // default to due date for all other cases
-    tasks = await prisma.task.findMany({
-      where: {
-        userId: session.user.id,
-        status: {
-          not: "COMPLETED",
-        },
-        dueDate: {
-          gte: new Date(new Date().setHours(0, 0, 0, 0)),
-          lte: new Date(new Date().setHours(23, 59, 59, 999)),
-        },
-      },
-      orderBy: {
-        listOrder: "asc",
-      },
-    });
-  }
+  const tasks = await getTasksByFilter(searchParams?.filter);
 
   return (
     <TaskList
