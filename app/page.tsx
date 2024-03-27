a8bec3835655e682e39f64c55e94997496448b3a
Norbu Sonam <43245807+norbusonam@users.noreply.github.com>
2024-03-26 21:19:29 -0700

    login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,69 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Moon, Sun } from "lucide-react";
-import { useTheme } from "next-themes";
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function Page() {
-  const { setTheme } = useTheme();
-
   return (
-    <div>
-      <h1 className="text-2xl">Taskbrew</h1>
-      <Button>Get Started</Button>
-
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="outline" size="icon">
-            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            <span className="sr-only">Toggle theme</span>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuItem onClick={() => setTheme("light")}>
-            Light
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setTheme("dark")}>
-            Dark
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setTheme("system")}>
-            System
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+    <div className="flex items-center justify-center h-screen">
+      <Tabs defaultValue="log-in" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="log-in">Login</TabsTrigger>
+          <TabsTrigger value="sign-up">Password</TabsTrigger>
+        </TabsList>
+        <TabsContent value="log-in">
+          <Card>
+            <CardHeader>
+              <CardTitle>Login</CardTitle>
+              <CardDescription>Enter your credentials</CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="username">Username</Label>
+                <Input id="username" placeholder="Username" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" placeholder="Password" type="password" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Log in</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+        <TabsContent value="sign-up">
+          <Card>
+            <CardHeader>
+              <CardTitle>Sign up</CardTitle>
+              <CardDescription>
+                Create an account to get started.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="username">Username</Label>
+                <Input id="username" placeholder="Username" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" placeholder="Password" type="password" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Sign up</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+      </Tabs>
     </div>
   );
 }
