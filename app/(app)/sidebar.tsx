23eb3d2398946232bcb4396edb1f8fab1002a9d3
Norbu Sonam <43245807+norbusonam@users.noreply.github.com>
2024-03-31 14:44:39 -0700

    sidebar links

--- app/(app)/sidebar.tsx
+++ app/(app)/sidebar.tsx
@@ -1,3 +1,18 @@
+import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
 export function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Taskbrew</h1>
+      <div className="flex flex-col">
+        <Link className={buttonVariants({ variant: "ghost" })} href="/home">
+          Home
+        </Link>
+        <Link className={buttonVariants({ variant: "ghost" })} href="/tasks">
+          Tasks
+        </Link>
+      </div>
+    </div>
+  );
 }
