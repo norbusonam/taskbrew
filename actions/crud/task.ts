cd3441c52af5d965c727626bd3a4e278511f77c0
Norbu Sonam <43245807+norbusonam@users.noreply.github.com>
2024-04-29 21:42:42 -0700

    list all tasks

--- actions/crud/task.ts
+++ actions/crud/task.ts
@@ -5,3 +5,7 @@ import { NewTask, db } from "@/lib/database";
 export async function createTask(task: NewTask) {
   await db.insertInto("task").values(task).execute();
 }
+
+export async function getTasks() {
+  return await db.selectFrom("task").selectAll().execute();
+}
--- app/(app)/tasks/page.tsx
+++ app/(app)/tasks/page.tsx
@@ -1,10 +1,20 @@
+import { getTasks } from "@/actions/crud/task";
 import { NewTaskButton } from "@/components/new-task-button";
 
-export default function Page() {
+export default async function Page() {
+  const tasks = await getTasks();
+
   return (
     <div>
       <h1 className="text-2xl">Tasks</h1>
-      <p>This is the tasks page.</p>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <h2>{task.name}</h2>
+            <p>{task.description}</p>
+          </li>
+        ))}
+      </ul>
       <NewTaskButton />
     </div>
   );
