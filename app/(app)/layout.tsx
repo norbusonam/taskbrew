dd24bae669feed67b537f317bdcdc5b44e0645fb
Norbu Sonam <43245807+norbusonam@users.noreply.github.com>
2024-05-16 23:53:08 -0400

    fix layout, put tasks in table

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -2,9 +2,9 @@ import { Sidebar } from "./sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-row h-screen bg-muted/40">
+    <div className="flex bg-muted/40 h-dvh w-screen">
       <Sidebar />
-      {children}
+      <main className="flex-grow">{children}</main>
     </div>
   );
 }
--- app/(app)/tasks/page.tsx
+++ app/(app)/tasks/page.tsx
@@ -1,21 +1,42 @@
 import { getTasks } from "@/actions/crud/task";
 import { NewTaskButton } from "@/components/new-task-button";
+import { Card } from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export default async function Page() {
   const tasks = await getTasks();
 
   return (
-    <div>
-      <h1 className="text-2xl">Tasks</h1>
-      <ul>
-        {tasks.map((task) => (
-          <li key={task.id}>
-            <h2>{task.name}</h2>
-            <p>{task.description}</p>
-          </li>
-        ))}
-      </ul>
-      <NewTaskButton />
+    <div className="flex flex-col h-full w-full p-4 space-y-4">
+      <div className="flex justify-between items-center">
+        <h1 className="text-2xl">Tasks</h1>
+        <NewTaskButton />
+      </div>
+      <Card>
+        <Table className="w-full">
+          <TableHeader>
+            <TableRow>
+              <TableHead>Name</TableHead>
+              <TableHead>Description</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {tasks.map((task) => (
+              <TableRow key={task.id}>
+                <TableCell>{task.name}</TableCell>
+                <TableCell>{task.description}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Card>
     </div>
   );
 }
